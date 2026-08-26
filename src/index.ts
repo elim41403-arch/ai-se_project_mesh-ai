4769c705cb94553dd7a0a60b49493b7013cba677
Ethan L <elim41403@gmail.com>
2026-08-26 14:36:05 -0700

    feat: added middleware (logger and error handlers)

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
 import express from 'express';
 import router from './routes/index.js';
+import { logger } from './middleware/loggers.js';
+import { errorHandler, notFoundHandler } from './middleware/error.js';
 
 const app = express();
 
+app.use(express.json());
+app.use(logger);
 app.use(router);
+
+app.get('/test-error', (req, res) => {
+  throw new Error('Test error');
+});
 
 app.get("/health", (req, res): void => {
     res.status(200).json({
@@ -12,6 +20,9 @@ app.get("/health", (req, res): void => {
         "error": null
     });
 });
+
+app.use(notFoundHandler);
+app.use(errorHandler);
 
 const port = 3000;
 app.listen(port, () =>{
