14a64f7234dc16ca830f56e4e8906c015f32994c
Ethan L <elim41403@gmail.com>
2026-08-27 04:53:33 -0700

    docs: fixed handlers

--- src/middleware/error.ts
+++ src/middleware/error.ts
@@ -1,4 +1,4 @@
-import type { Request, Response, NextFunction } from "express";
+import type { Request, Response } from "express";
 
 function errorHandler( 
     err: Error & { statusCode?: number },
